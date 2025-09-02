c0ad9ac6476ccc4e129920a3203f8bcf6839d793
HyungKu Im <damansa1@naver.com>
2025-09-02 20:37:09 +0900

    9/2 수업내용(배열까지)

--- Proj1/src/Sample008.java
+++ Proj1/src/Sample008.java
@@ -11,9 +11,16 @@ public class Sample008 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String[] movies = {
-			"터미네이터", "아바타", "타이타닉", "스파이더맨"
-		};
+//		String[] movies = {
+//			"터미네이터", "아바타", "타이타닉", "스파이더맨"
+//		};
+		
+		String[] movies = new String[4];  // 4편의 영화 배열 생성
+		movies[0] = "터미네이터";
+		movies[1] = "아바타";
+		movies[2] = "타이타닉";
+		movies[3] = "스파이더맨";
+		//movies[4] = "영화";
 		
 		for (int i = 0; i < 2; i++) {
 			System.out.println(movies[i]);
